47f40fc87fae234ceb347d1a15117b5bce689fc3
Rataysh <kiril.ebarev@gmail.com>
2022-04-30 16:04:36 +0300

    eng_project

--- js/my_app.js
+++ js/my_app.js
@@ -1,1 +1,12 @@
-alert('Hello!')
+// alert('Hello!')
+// result = prompt('title', ['default'])
+
+// // Создание переменных:
+// const firstTestVar = "You can NOT change this"
+// let secondTestVar = "You can change this"
+
+// // firstTestVar = "I'm trying change" // eror in console
+// secondTestVar = "I'm trying change"
+
+// console.log(firstTestVar, secondTestVar)
+
