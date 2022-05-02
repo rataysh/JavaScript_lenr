cf6dc255983fe73a16668c4e819c7e6a148dc458
Rataysh <kiril.ebarev@gmail.com>
2022-05-02 13:14:07 +0300

    old_version

--- js/my_app.js
+++ js/my_app.js
@@ -1,11 +1,27 @@
 const buttonStart = document.getElementById('start-button')
 const listSubject = ['Lida', 'Denis', 'Bebyrishvili', 'GID', 'Chernaya Marina']
-
+const resultElementDom = document.getElementsByClassName('resultElem')
+const listClassesResultElem = ['resultElem', 'resultElem1', 'resultElem2', 'resultElem3']
+// let randomIndex = Math.floor(Math.random() * listSubject.length)
 
 function generateNewEx() {
     let randomIndex = Math.floor(Math.random() * listSubject.length)
-    alert(listSubject[randomIndex])
+    let randomClasses = Math.floor(Math.random() * listClassesResultElem.length)
+    
+    let choiceClass = document.getElementsByClassName(listClassesResultElem[randomClasses])
+    choiceClass[0].innerHTML = listSubject[randomIndex]
+
+    // resultElementDom[0].innerHTML = listSubject[randomIndex]
+    console.log(listClassesResultElem[randomClasses])
+    // console.log(choiceClass)
+
+    // for (let i = 0; i < resultElementDom.length; i++) {
+    //     resultElementDom[i].innerHTML = listSubject[randomIndex]
+    // } 
+    // resultElementDom.innerHTML = listSubject[randomIndex]
 }
+
+
 
 buttonStart.onclick = generateNewEx
 // Ниже аналогичная запись, только через стрелочную функцию
@@ -13,4 +29,6 @@ buttonStart.onclick = generateNewEx
 //     alert('Нажатие на кнопку работает!')
 // }
 
-console.log(listSubject[randomIndex])
+
+// console.log(resultElementDom)
+
