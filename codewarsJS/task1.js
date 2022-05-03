c046b4080d338906479ec5f54baee96f7408d423
Rataysh <kiril.ebarev@gmail.com>
2022-05-03 12:24:26 +0300

    complite

--- codewarsJS/task1.js
+++ codewarsJS/task1.js
@@ -5,7 +5,15 @@
 // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
-textNotJaden = "How can mirrors be real if our eyes aren't real"
-listWithNotJadenText = textNotJaden.split(' ')
+let textNotJaden = "How can mirrors be real if our eyes aren't real"
 
-console.log(listWithNotJadenText)
+function textJadenFunc(inputText) {
+    let listWithNotJadenText = inputText.split(' ')
+    let listJaden = listWithNotJadenText.map(element => element[0].toUpperCase() + element.substring(1))
+    return listJaden.join(' ')
+    // let newList = listWithNotJadenText.map(element => {
+    //     return element.toUpperCase() + element.substring(1)
+    // })
+}
+
+textJadenFunc(textNotJaden)
