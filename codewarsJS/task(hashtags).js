8057a651f734aa5972d1b4f8411c436739831f52
Rataysh <kiril.ebarev@gmail.com>
2022-05-05 12:30:21 +0300

    hashtag

---
+++ codewarsJS/task(hashtags).js
@@ -0,0 +1,29 @@
+// The marketing team is spending way too much time typing in hashtags.
+// Let's help them with our own Hashtag Generator!
+// Here's the deal:
+// It must start with a hashtag (#).
+// All words must have their first letter capitalized.
+// If the final result is longer than 140 chars it must return false.
+// If the input or the result is an empty string it must return false.
+// Examples
+// " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+// "    Hello     World   "                  =>  "#HelloWorld"
+// ""                                        =>  false
+
+let testData = "Codewars"
+
+
+function generateHashtag(str) {
+    if (str === "") {
+        return false
+    } else if (str === " ") {
+        return false
+    } else {
+        let new_array = str.trim().split(' ').map((a) => a.charAt(0).toUpperCase() + a.slice(1))
+        let result = (new_array.join('').length < 140) ? '#' + new_array.join('') : false
+        return result
+    }
+}
+
+
+console.log(generateHashtag(testData))
