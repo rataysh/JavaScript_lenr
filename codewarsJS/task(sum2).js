04d59e62408bd9e6ff4c3e75258da43f4fed5f99
Rataysh <kiril.ebarev@gmail.com>
2022-05-08 22:48:46 +0300

    sum2

---
+++ codewarsJS/task(sum2).js
@@ -0,0 +1,11 @@
+let testData = '             1                        3     5                        7     9    11                       13    15    17    19'
+
+
+function rowSumOddNumbers(n) {
+	let listData = n.split(' ')
+    .map(element => (element === '') ? 0: element)
+    .reduce((sum, elem) => Number(sum) + Number(elem))
+    return listData
+}
+
+rowSumOddNumbers(testData)
