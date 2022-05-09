090c12ed167dfe7e6a453dada12ee25d89f4bf1b
Rataysh <kiril.ebarev@gmail.com>
2022-05-09 17:32:08 +0300

    not result

--- codewarsJS/task(CountPositivesSumNegatives).js
+++ codewarsJS/task(CountPositivesSumNegatives).js
@@ -1,4 +1,4 @@
-let testData = [4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+let testData = [4, 5, -11, 7, 8, 9, 10, -11, -12, -13, -14, -15];
 
 function countPositivesSumNegatives(input) {
     let listPlus = [];
