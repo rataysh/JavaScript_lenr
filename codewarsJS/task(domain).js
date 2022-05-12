35f690124ac5512f8a6288707ecc893a6b08b68b
Rataysh <kiril.ebarev@gmail.com>
2022-05-12 15:01:02 +0300

    regex

---
+++ codewarsJS/task(domain).js
@@ -0,0 +1,21 @@
+// Write a function that when given a URL as a string, parses out just the domain name and 
+// returns it as a string. For example:
+
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = cnet"
+
+
+let testData = "https://www.qgq3g7-ss.com/warez/" 
+
+
+function domainName(url){
+    return url.replace(/(\w+\:\/\/)?(www\.)?/ig, '').replace(/(\..+)/ig, '')
+}
+
+
+
+
+
+
+domainName(testData)
