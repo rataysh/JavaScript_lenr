65fdae6d3e5af4982a6fae97253bfe31048d5806
Rataysh <kiril.ebarev@gmail.com>
2022-05-23 20:04:01 +0300

    marginHome

--- reactNative/expoTest/myApp/components/Home.js
+++ reactNative/expoTest/myApp/components/Home.js
@@ -5,15 +5,33 @@ import Icon from "react-native-vector-icons/Ionicons";
 export default function Home() {
     return (
         <View style={[styles.container]}>
-            {/* <Text style={styles.text}>HELLO MY HOME!!!</Text> */}
-            <View style={styles.square}>
+            <View style={[styles.square]}>
                 <Text style={styles.text}>HELLO MY HOME!!!</Text>
                 <Icon name="home" size={30} color="#4F8EF7" />
             </View>
-            <View style={styles.btn}>
-                <View style={styles.btn}>
-
-                </View>
+            <View
+                style={[
+                    styles.btnRules,
+                    {
+                        justifyContent: "center",
+                        flexDirection: "row",
+                        alignItems: "center",
+                    },
+                ]}
+            >
+                <Text style={styles.text}>RULES</Text>
+            </View>
+            <View
+                style={[
+                    styles.btnStart,
+                    {
+                        justifyContent: "center",
+                        flexDirection: "row",
+                        alignItems: "center",
+                    },
+                ]}
+            >
+                <Text style={styles.text}>START</Text>
             </View>
         </View>
     );
@@ -21,33 +39,39 @@ export default function Home() {
 
 const styles = StyleSheet.create({
     container: {
-        position: "relative",
+        flex: 1,
+        backgroundColor: "rgba(29, 33, 46, 0.69)",
+        padding: '20%',
+        // position: "relative",
         // width: Dimensions.get("window").width,
-        height: 800,
-        borderRadius: 0,
-        overflow: "hidden",
-        transform: [{ translateX: 0 }, { translateY: 0 }, { rotate: "0deg" }],
-        backgroundColor: "rgba(29, 33, 46, 0.6899999976158142)",
-        left: 0,
-        top: 0,
-    },
-    text: {
-        fontSize: 25,
+        // height: 800,
+        // borderRadius: 0,
+        // overflow: "hidden",
+        // transform: [{ translateX: 0 }, { translateY: 0 }, { rotate: "0deg" }],
+        // padding: 'spacing',
+        // flexDirection: "column",
+        // flexWrap:'wrap',
+        // aliginContent: 'stretch',
+        // alignItems: "center",
+        // justifyContent: "center",
+        // left: 0,
+        // top: 0,
     },
     square: {
         position: "relative",
+        // flex: 1,
         width: 180,
         height: 180,
         borderRadius: 30,
         alignSelf: "center",
         // flexDirection: "row",
-        marginTop: "15%",
+        // marginTop: "15%",
         opacity: 1,
         borderWidth: 1,
         borderColor: "rgba(236, 42, 42, 0.76)",
-        right: "auto",
-        bottom: "auto",
-        transform: [{ translateX: 0 }, { translateY: 0 }, { rotate: "0deg" }],
+        // right: "auto",
+        // bottom: "auto",
+        // transform: [{ translateX: 0 }, { translateY: 0 }, { rotate: "0deg" }],
         shadowColor: "rgba(0, 0, 0, 0.25)",
         shadowOffset: {
             width: 0,
@@ -60,19 +84,16 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
-    btn: {
+    btnRules: {
         position: "relative",
         width: 180,
         height: 50,
         alignSelf: "center",
-        // flexDirection: "row",
-        marginTop: "65%",
-        marginBottom: "20%",
+        marginTop: "70%",
         borderRadius: 30,
         opacity: 1,
         borderWidth: 1,
         borderColor: "rgba(229, 179, 179, 0.26)",
-        transform: [{ translateX: 0 }, { translateY: 0 }, { rotate: "0deg" }],
         shadowColor: "rgb(0, 0, 0)",
         shadowOffset: {
             width: 0,
@@ -81,6 +102,34 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 4,
         backgroundColor: "rgba(95, 116, 230, 1)",
+    },
+    btnStart: {
+        position: "relative",
+        width: 180,
+        height: 50,
+        alignSelf: "center",
+        marginTop: "10%",
+        borderRadius: 30,
+        opacity: 1,
+        borderWidth: 1,
+        borderColor: "rgba(229, 179, 179, 0.26)",
+        shadowColor: "rgb(0, 0, 0)",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        backgroundColor: "rgba(95, 116, 230, 1)",
+    },
+    text: {
+        fontFamily: "OpenSans-ExtraBold",
+        textDecorationLine: "none",
+        fontSize: 25,
+        color: "rgba(252, 252, 252, 0.86)",
+        textAlign: "left",
+        textAlignVertical: "top",
+        letterSpacing: 0.1,
     },
 });
 
